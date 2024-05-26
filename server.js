c99e7b4e295b31520ec6d5c4fb47c577a7bf4e60
Corwnbot <123889850+Corwnbot@users.noreply.github.com>
2024-05-26 23:02:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -21,14 +21,29 @@ setInterval(() => {
 }, 280000);
 
 
-const { Client } = require('discord.js-selfbot-v13');
+const { Client, RichPresence } = require('discord.js-selfbot-v13');
 const client = new Client();
 
 const { joinVoiceChannel } = require('@discordjs/voice');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
-    client.user.setActivity('By 18-81', { type: 'PLAYING' });
+    const startDate = new Date();
+  const formattedDate = startDate.toLocaleString();
+
+const a1881 = new RichPresence()
+
+.setApplicationId(`1179783460135649392`)
+
+.setType(`PLAYING`)
+
+.setName(`PojavLauncher`)
+
+.setState(`Now Playing At ${formattedDate}`)
+
+.setAssetsLargeImage('https://cdn.discordapp.com/avatars/1179783460135649392/341a716622e9b45f70f76ada1076ae9c.webp?size=2048')
+
+client.user.setActivity(a1881)
 });
 
 client.login(process.env.t);
